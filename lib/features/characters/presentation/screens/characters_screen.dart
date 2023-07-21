c2998225d941ebdb14599476afc96d246c2a7395
Hector Montilla <elcama_86@msn.com>
2023-07-21 13:09:54 -0400

    scaffold modificado y uso de bloc en screen

--- lib/features/characters/presentation/screens/characters_screen.dart
+++ lib/features/characters/presentation/screens/characters_screen.dart
@@ -1,23 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'package:rick_and_morty_app/features/characters/presentation/blocs/blocs.dart';
 import 'package:rick_and_morty_app/features/characters/presentation/views/views.dart';
+import 'package:rick_and_morty_app/features/shared/widgets/widgets.dart';
 
 class CharactersScreen extends StatelessWidget {
   const CharactersScreen({super.key});
 
+  void showSnackbar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Personajes"),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () {},
-          ),
-        ],
+      body: BlocListener<CharactersBloc, CharactersState>(
+        bloc: BlocProvider.of<CharactersBloc>(context),
+        listener: (context, state) {
+          if (state.errorMessage.isNotEmpty) {
+            showSnackbar(context, state.errorMessage);
+          }
+        },
+        child: BlocBuilder<CharactersBloc, CharactersState>(
+          bloc: BlocProvider.of<CharactersBloc>(context),
+          builder: (context, state) {
+            if (state.isLoading && state.characters.isEmpty) {
+              return const ScaffoldLoading();
+            }
+
+            return CharactersView(
+              characters: state.characters,
+              loadNextPage: context.read<CharactersBloc>().loadNextPage,
+            );
+          },
+        ),
       ),
-      body: const CharactersView(),
     );
   }
 }
