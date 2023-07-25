d376818f1613940f1929bdc3de7612bdac3dab49
Hector Montilla <elcama_86@msn.com>
2023-07-25 17:55:55 -0400

    creacion de utilidad compartida showSnackbar

---
+++ lib/features/shared/utils/utils.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Utils {
+  static void showSnackbar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+      ),
+    );
+  }
+}
