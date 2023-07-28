463c50db7a2bf274d2ab6b2da7e13d12cd10c7c7
Hector Montilla <elcama_86@msn.com>
2023-07-28 16:30:25 -0400

    episode card creado

---
+++ lib/features/episodes/presentation/widgets/episode_card.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:rick_and_morty_app/features/episodes/episodes.dart';
+
+class EpisodeCard extends StatelessWidget {
+  final Episode episode;
+
+  const EpisodeCard({
+    super.key,
+    required this.episode,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final colors = Theme.of(context).colorScheme;
+    final textStyle = Theme.of(context).textTheme.titleMedium;
+
+    return SizedBox(
+      width: size.width * 0.3,
+      height: 165.0,
+      child: Card(
+        clipBehavior: Clip.antiAlias,
+        elevation: 30.0,
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+              colors.surfaceVariant,
+              EpisodeUtils.colorBySeason(episode.episode),
+            ]),
+          ),
+          child: Center(
+            child: Text(
+              'Episodio ${episode.id}',
+              textAlign: TextAlign.center,
+              style: textStyle,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/episodes/presentation/widgets/widgets.dart
+++ lib/features/episodes/presentation/widgets/widgets.dart
@@ -1,2 +1,3 @@
+export 'episode_card.dart';
 export 'episode_poster.dart';
 export 'episode_searched_item.dart';
