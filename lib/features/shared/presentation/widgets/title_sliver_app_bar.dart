2e5639e6f2da270d5756d2de3d5782130437a7d0
Hector Montilla <elcama_86@msn.com>
2023-07-28 16:30:00 -0400

    title sliver appbar creado

---
+++ lib/features/shared/presentation/widgets/title_sliver_app_bar.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:rick_and_morty_app/features/shared/shared.dart';
+
+class TitleSliverAppBar extends StatelessWidget {
+  final String title;
+  final TextStyle? textStyle;
+  final Color gradientColor;
+  final double bottom;
+  final double left;
+
+  const TitleSliverAppBar({
+    super.key,
+    required this.title,
+    required this.textStyle,
+    required this.gradientColor,
+    required this.bottom,
+    required this.left,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: [
+        CustomGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          stops: const [0.5, 1.0],
+          colors: [
+            Colors.transparent,
+            gradientColor,
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.only(bottom: 8.0),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(),
+              const Spacer(),
+              Expanded(
+                flex: 5,
+                child: Text(
+                  title,
+                  style: textStyle,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/features/shared/presentation/widgets/widgets.dart
+++ lib/features/shared/presentation/widgets/widgets.dart
@@ -4,3 +4,4 @@ export 'elements_mansory.dart';
 export 'loading_spinner.dart';
 export 'scaffold_background.dart';
 export 'scaffold_screen.dart';
+export 'title_sliver_app_bar.dart';
