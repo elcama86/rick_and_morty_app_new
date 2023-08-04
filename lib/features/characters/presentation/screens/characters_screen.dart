7658827f80afa72ef2fba921ac44f85c7e5ab42d
Hector Montilla <elcama_86@msn.com>
2023-08-04 11:15:03 -0400

    _scaffoldScreen and _scaffoldBody created

--- lib/features/characters/presentation/screens/characters_screen.dart
+++ lib/features/characters/presentation/screens/characters_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rick_and_morty_app/features/characters/domain/domain.dart';
 
 import 'package:rick_and_morty_app/features/characters/presentation/blocs/blocs.dart';
+import 'package:rick_and_morty_app/features/characters/presentation/views/views.dart';
 import 'package:rick_and_morty_app/features/shared/shared.dart';
 
 class CharactersScreen extends StatefulWidget {
@@ -32,12 +34,60 @@ class _CharactersScreenState extends State<CharactersScreen> {
       child: BlocBuilder<CharactersBloc, CharactersState>(
         bloc: BlocProvider.of<CharactersBloc>(context),
         builder: (context, state) {
-          return ScaffoldScreen(
-            elements: state.characters,
+          return _ScaffoldScreen(
+            characters: state.characters,
             isLoading: state.isLoading,
           );
         },
       ),
+    );
+  }
+}
+
+class _ScaffoldScreen extends StatelessWidget {
+  final List<Character> characters;
+  final bool isLoading;
+  const _ScaffoldScreen({
+    required this.characters,
+    required this.isLoading,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: Utils.appBarContain(characters, "Personajes"),
+      body: _ScaffoldBody(
+        characters: characters,
+        isLoading: isLoading,
+      ),
+    );
+  }
+}
+
+class _ScaffoldBody extends StatelessWidget {
+  final List<Character> characters;
+  final bool isLoading;
+
+  const _ScaffoldBody({
+    required this.characters,
+    required this.isLoading,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (isLoading && characters.isEmpty) {
+      return const LoadingSpinner();
+    }
+
+    if (characters.isEmpty) {
+      return const CustomMessage(
+        message: "No existen personajes cargados",
+      );
+    }
+
+    return CharactersView(
+      characters: characters,
+      loadNextPage: context.read<CharactersBloc>().loadNextPage,
     );
   }
 }
