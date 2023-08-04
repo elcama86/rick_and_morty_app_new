e79eb879549ad900102791f214d334bc835f50f9
Hector Montilla <elcama_86@msn.com>
2023-08-04 11:09:39 -0400

    isar variable created

--- lib/features/episodes/infrastructure/datasources/local_storage_datasource_impl.dart
+++ lib/features/episodes/infrastructure/datasources/local_storage_datasource_impl.dart
@@ -3,47 +3,55 @@ import 'package:path_provider/path_provider.dart';
 import 'package:rick_and_morty_app/features/episodes/domain/domain.dart';
 
 class LocalStorageDatasourceImpl extends LocalStorageDatasource {
-  Isar? isar;
+  late Future<Isar> isar;
 
   LocalStorageDatasourceImpl() {
-    openIsar();
+    isar = openIsar();
   }
 
-  void openIsar() async {
+  Future<Isar> openIsar() async {
     final dir = await getApplicationDocumentsDirectory();
 
-    isar ??= await Isar.open(
-      [EpisodeSchema],
-      directory: dir.path,
-    );
+    if (Isar.instanceNames.isEmpty) {
+      return await Isar.open(
+        [EpisodeSchema],
+        inspector: true,
+        directory: dir.path,
+      );
+    }
+
+    return Future.value(Isar.getInstance());
   }
 
   @override
   Future<bool> isEpisodeFavorite(int episodeId) async {
+    final db = await isar;
+
     final Episode? favoriteEpisode =
-        await isar!.episodes.filter().idEqualTo(episodeId).findFirst();
+        await db.episodes.filter().idEqualTo(episodeId).findFirst();
+
     return favoriteEpisode != null;
   }
 
   @override
-  Future<List<Episode>> loadEpisodes({int limit = 20, int offset = 0}) {
-    return isar!.episodes.where().offset(offset).limit(limit).findAll();
+  Future<List<Episode>> loadEpisodes({int limit = 20, int offset = 0}) async {
+    final db = await isar;
+
+    return db.episodes.where().offset(offset).limit(limit).findAll();
   }
 
   @override
   Future<void> toggleFavorite(Episode episode) async {
+    final db = await isar;
+
     final Episode? favoriteEpisode =
-        await isar!.episodes.filter().idEqualTo(episode.id).findFirst();
+        await db.episodes.filter().idEqualTo(episode.id).findFirst();
 
     if (favoriteEpisode != null) {
-      await isar!.writeTxn(() async {
-        await isar!.episodes.delete(favoriteEpisode.isarId);
-        return;
-      });
+      db.writeTxnSync(() => db.episodes.deleteSync(favoriteEpisode.isarId));
+      return;
     }
 
-    await isar!.writeTxn(() async {
-      await isar!.episodes.put(episode);
-    });
+    db.writeTxnSync(() => db.episodes.putSync(episode));
   }
 }
