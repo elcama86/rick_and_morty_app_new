4d664b81ec045024db80da78d45db034b7e0a726
Hector Montilla <elcama_86@msn.com>
2023-08-04 11:18:44 -0400

    load next page implemented

--- lib/features/episodes/presentation/views/episodes_view.dart
+++ lib/features/episodes/presentation/views/episodes_view.dart
@@ -2,18 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:animate_do/animate_do.dart';
+import 'package:go_router/go_router.dart';
 import 'package:rick_and_morty_app/features/episodes/episodes.dart';
-import 'package:rick_and_morty_app/features/shared/presentation/delegates/search_elements_delegate.dart';
-import 'package:rick_and_morty_app/features/shared/presentation/widgets/widgets.dart';
+import 'package:rick_and_morty_app/features/shared/shared.dart';
 
 class EpisodesView extends StatefulWidget {
-  final List<Episode> episodes;
-  final VoidCallback? loadNextPage;
-
   const EpisodesView({
     super.key,
-    required this.episodes,
-    this.loadNextPage,
   });
 
   @override
@@ -21,6 +16,61 @@ class EpisodesView extends StatefulWidget {
 }
 
 class _EpisodesViewState extends State<EpisodesView> {
+  @override
+  void initState() {
+    super.initState();
+    final episodesState = context.read<EpisodesBloc>().state;
+    if (episodesState.episodes.isEmpty) {
+      context.read<EpisodesBloc>().loadNextPage();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<EpisodesBloc, EpisodesState>(
+      bloc: BlocProvider.of<EpisodesBloc>(context),
+      listener: (context, state) {
+        if (state.errorMessage.isNotEmpty && !state.isLoading) {
+          Utils.showSnackbar(context, state.errorMessage);
+        }
+      },
+      child: BlocBuilder<EpisodesBloc, EpisodesState>(
+        bloc: BlocProvider.of<EpisodesBloc>(context),
+        builder: (context, state) {
+          if (state.isLoading && state.episodes.isEmpty) {
+            return const LoadingSpinner();
+          }
+
+          if (state.episodes.isEmpty) {
+            return const CustomMessage(
+              message: "No existen episodios cargados",
+            );
+          }
+
+          return _EpisodesViewContain(
+            episodes: state.episodes,
+            loadNextPage: context.read<EpisodesBloc>().loadNextPage,
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _EpisodesViewContain extends StatefulWidget {
+  final List<Episode> episodes;
+  final VoidCallback? loadNextPage;
+
+  const _EpisodesViewContain({
+    required this.episodes,
+    required this.loadNextPage,
+  });
+
+  @override
+  State<_EpisodesViewContain> createState() => _EpisodesViewContainState();
+}
+
+class _EpisodesViewContainState extends State<_EpisodesViewContain> {
   final controller = ScrollController();
 
   @override
@@ -60,16 +110,20 @@ class _EpisodesViewState extends State<EpisodesView> {
               style: appBarTitleTheme,
             ),
           ),
+          leading: IconButton(
+            onPressed: () => context.pop(),
+            icon: const Icon(Icons.arrow_back),
+          ),
           actions: [
             FadeIn(
               child: IconButton(
                 onPressed: () {
                   final searchEpisodesState =
                       context.read<SearchEpisodesBloc>().state;
-            
+
                   final searchEpisodes =
                       context.read<SearchEpisodesBloc>().searchEpisodesByQuery;
-            
+
                   showSearch<Episode?>(
                     query: searchEpisodesState.query,
                     context: context,
