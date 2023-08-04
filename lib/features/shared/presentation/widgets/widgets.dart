c17ad0fb8c744e12a07fc59486a3d31987357610
Hector Montilla <elcama_86@msn.com>
2023-08-04 11:12:03 -0400

    scaffold screen removed

--- lib/features/shared/presentation/widgets/widgets.dart
+++ lib/features/shared/presentation/widgets/widgets.dart
@@ -1,3 +1,4 @@
+export 'custom_bottom_navigation.dart';
 export 'custom_gradient.dart';
 export 'custom_message.dart';
 export 'element_horizontal_listview.dart';
@@ -5,5 +6,4 @@ export 'elements_by_entity.dart';
 export 'elements_mansory.dart';
 export 'loading_spinner.dart';
 export 'scaffold_background.dart';
-export 'scaffold_screen.dart';
 export 'title_sliver_app_bar.dart';
