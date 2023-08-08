010fe4821f8a311bfdf21dc2e816f65bba41b6f8
Hector Montilla <elcama_86@msn.com>
2023-08-08 11:37:56 -0400

    animation to show/hide bottom nav bar using cubit

--- lib/features/shared/presentation/widgets/custom_bottom_navigation.dart
+++ lib/features/shared/presentation/widgets/custom_bottom_navigation.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rick_and_morty_app/features/shared/shared.dart';
 
 class CustomBottomNavigation extends StatelessWidget {
   final int index;
@@ -13,25 +15,34 @@ class CustomBottomNavigation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final colors = Theme.of(context).colorScheme;
+    final bottomNavBarState = context.watch<BottomNavBarCubit>().state;
 
-    return BottomNavigationBar(
-      elevation: 16.0,
-      currentIndex: index,
-      onTap: onItemTapped,
-      selectedItemColor: colors.primary,
-      type: BottomNavigationBarType.shifting,
-      showUnselectedLabels: true,
-      unselectedItemColor: colors.secondary,
-      items: const [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.video_label_outlined),
-          label: 'Inicio',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.favorite_outline),
-          label: 'Favoritos',
-        ),
-      ],
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 200),
+      height: bottomNavBarState.isVisible ? kBottomNavigationBarHeight : 0.0,
+      child: Wrap(
+        children: [
+          BottomNavigationBar(
+            elevation: 16.0,
+            currentIndex: index,
+            onTap: onItemTapped,
+            selectedItemColor: colors.primary,
+            type: BottomNavigationBarType.shifting,
+            showUnselectedLabels: true,
+            unselectedItemColor: colors.secondary,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.video_label_outlined),
+                label: 'Inicio',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.favorite_outline),
+                label: 'Favoritos',
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
