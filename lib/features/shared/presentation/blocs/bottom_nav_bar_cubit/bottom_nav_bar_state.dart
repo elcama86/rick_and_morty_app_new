4b6ccbcd9830731d0aae8e283131a54501a13eff
Hector Montilla <elcama_86@msn.com>
2023-08-10 12:56:41 -0400

    index, appBarHeight and scrollPositions added

--- lib/features/shared/presentation/blocs/bottom_nav_bar_cubit/bottom_nav_bar_state.dart
+++ lib/features/shared/presentation/blocs/bottom_nav_bar_cubit/bottom_nav_bar_state.dart
@@ -1,19 +1,28 @@
 part of 'bottom_nav_bar_cubit.dart';
 
-class BottomNavBarState extends Equatable {
+class BottomNavBarState {
+  final int currentIndex;
   final bool isVisible;
+  final double appBarHeight;
+  final Map<int, double> scrollPositions;
 
   const BottomNavBarState({
+    this.currentIndex = 0,
     this.isVisible = true,
+    this.appBarHeight = kToolbarHeight,
+    this.scrollPositions = const {},
   });
 
   BottomNavBarState copyWith({
+    int? currentIndex,
     bool? isVisible,
+    double? appBarHeight,
+    Map<int, double>? scrollPositions,
   }) =>
       BottomNavBarState(
+        currentIndex: currentIndex ?? this.currentIndex,
         isVisible: isVisible ?? this.isVisible,
+        appBarHeight: appBarHeight ?? this.appBarHeight,
+        scrollPositions: scrollPositions ?? this.scrollPositions,
       );
-
-  @override
-  List<Object> get props => [isVisible];
 }
