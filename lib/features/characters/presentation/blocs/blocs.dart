626fafac39722bf0c179311843b64f1b94506f18
Hector Montilla <elcama_86@msn.com>
2023-08-11 09:47:51 -0400

    characters slide cubit in barrel file

--- lib/features/characters/presentation/blocs/blocs.dart
+++ lib/features/characters/presentation/blocs/blocs.dart
@@ -1,4 +1,5 @@
 export 'character_bloc/character_bloc.dart';
 export 'characters_bloc/characters_bloc.dart';
+export 'characters_slide_cubit/characters_slide_cubit.dart';
 export 'episodes_by_character_bloc/episodes_by_character_bloc.dart';
 export 'search_characters_bloc/search_characters_bloc.dart';
