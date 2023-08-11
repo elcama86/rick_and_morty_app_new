797608487a7ec08f6cb820f8bfead82eecb7d2ea
Hector Montilla <elcama_86@msn.com>
2023-08-11 09:48:34 -0400

    characters slide cubit implemented

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,30 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rick_and_morty_app/features/characters/characters.dart';
 import 'package:rick_and_morty_app/features/home/home.dart';
+import 'package:rick_and_morty_app/features/shared/shared.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<CharactersSlideCubit>().getCharactersSlide();
+  }
 
   @override
   Widget build(BuildContext context) {
     final scaffoldKey = GlobalKey<ScaffoldState>();
 
-    return BlocProvider(
-      create: (_) => NavDrawerCubit(),
-      child: Scaffold(
-        key: scaffoldKey,
-        appBar: AppBar(
-          title: Image.asset(
-            'assets/images/app_bar_background.png',
-            height: kToolbarHeight - 4.0,
+    return BlocBuilder<CharactersSlideCubit, CharactersSlideState>(
+      builder: (context, state) {
+        if (state.isLoading) {
+          return const LoadingSpinner();
+        }
+
+        return BlocProvider(
+          create: (_) => NavDrawerCubit(),
+          child: Scaffold(
+            key: scaffoldKey,
+            appBar: AppBar(
+              title: Image.asset(
+                'assets/images/app_bar_background.png',
+                height: kToolbarHeight - 4.0,
+              ),
+              centerTitle: true,
+            ),
+            body: const HomeView(),
+            drawer: SideMenu(
+              scaffoldKey: scaffoldKey,
+            ),
           ),
-          centerTitle: true,
-        ),
-        body: const HomeView(),
-        drawer: SideMenu(
-          scaffoldKey: scaffoldKey,
-        ),
-      ),
+        );
+      },
     );
   }
 }
