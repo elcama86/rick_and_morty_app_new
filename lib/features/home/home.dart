ed3b6b0443d132a7a3fb119e3598ce502cea91a7
Hector Montilla <elcama_86@msn.com>
2023-08-11 12:08:12 -0400

    archivo de barril modificado

--- lib/features/home/home.dart
+++ lib/features/home/home.dart
@@ -1,4 +1,4 @@
+export 'presentation/blocs/nav_drawer_cubit/nav_drawer_cubit.dart';
 export 'presentation/screens/home_screen.dart';
 export 'presentation/views/home_view.dart';
-export 'presentation/widgets/side_menu.dart';
-export 'presentation/blocs/nav_drawer_cubit/nav_drawer_cubit.dart';
+export 'presentation/widgets/widgets.dart';
