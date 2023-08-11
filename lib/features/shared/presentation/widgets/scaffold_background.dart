3b774b05e30fde841ba4faf5aaceddf6c5d01dbb
Hector Montilla <elcama_86@msn.com>
2023-08-11 16:01:08 -0400

    container with background color removed

--- lib/features/shared/presentation/widgets/scaffold_background.dart
+++ lib/features/shared/presentation/widgets/scaffold_background.dart
@@ -10,21 +10,14 @@ class ScaffoldBackground extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    final backgroundColor = Theme.of(context).scaffoldBackgroundColor;
 
     return SizedBox.expand(
       child: Stack(
         children: [
-          Positioned(
-            child: Container(
-              color: backgroundColor,
-            ),
-          ),
           // Background with image
           Container(
             height: size.height,
             decoration: const BoxDecoration(
-              color: Colors.black,
               image: DecorationImage(
                 opacity: 0.4,
                 fit: BoxFit.fill,
