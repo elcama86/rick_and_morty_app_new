b91968d8329da9ad726873f4dd8e2163383d603e
Hector Montilla <elcama_86@msn.com>
2023-08-14 10:05:23 -0400

    isRetrying added

--- lib/features/characters/presentation/blocs/characters_slide_cubit/characters_slide_state.dart
+++ lib/features/characters/presentation/blocs/characters_slide_cubit/characters_slide_state.dart
@@ -3,25 +3,29 @@ part of 'characters_slide_cubit.dart';
 class CharactersSlideState extends Equatable {
   final bool isLoading;
   final bool hasError;
+  final bool isRetrying;
   final List<Character> characters;
 
   const CharactersSlideState({
     this.isLoading = true,
     this.hasError = false,
+    this.isRetrying = false,
     this.characters = const [],
   });
 
   CharactersSlideState copyWith({
     bool? isLoading,
     bool? hasError,
+    bool? isRetrying,
     List<Character>? characters,
   }) =>
       CharactersSlideState(
         isLoading: isLoading ?? this.isLoading,
         hasError: hasError ?? this.hasError,
+        isRetrying: isRetrying ?? this.isRetrying,
         characters: characters ?? this.characters,
       );
 
   @override
-  List<Object> get props => [isLoading, hasError, characters];
+  List<Object> get props => [isLoading, hasError, isRetrying, characters];
 }
