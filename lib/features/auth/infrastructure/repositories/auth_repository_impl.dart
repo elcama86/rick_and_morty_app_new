1a87d55d81dfc7ad55a143319b7e8f5ba0fcbf64
Hector Montilla <elcama_86@msn.com>
2023-08-16 09:58:35 -0400

    auth repository implemented

---
+++ lib/features/auth/infrastructure/repositories/auth_repository_impl.dart
@@ -0,0 +1,32 @@
+import '../../domain/domain.dart';
+
+class AuthRepositoryImpl extends AuthRepository {
+  final AuthDatasource datasource;
+
+  AuthRepositoryImpl(this.datasource);
+
+  @override
+  Future<void> logInWithEmailAndPassword(
+      {required String email, required String password}) {
+    return datasource.logInWithEmailAndPassword(
+        email: email, password: password);
+  }
+
+  @override
+  Future<void> logInWithGoogle() {
+    return datasource.logInWithGoogle();
+  }
+
+  @override
+  Future<void> logOut() {
+    return datasource.logOut();
+  }
+
+  @override
+  Future<void> signUp({required String email, required String password}) {
+    return datasource.signUp(email: email, password: password);
+  }
+
+  @override
+  Stream<User> get user => datasource.user;
+}
