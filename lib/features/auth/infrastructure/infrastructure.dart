7935fa2095aab02bc11163aef3d7788691ed12ff
Hector Montilla <elcama_86@msn.com>
2023-08-16 15:00:17 -0400

    inputs added to barrel file

--- lib/features/auth/infrastructure/infrastructure.dart
+++ lib/features/auth/infrastructure/infrastructure.dart
@@ -1,4 +1,5 @@
 export 'datasources/auth_datasource_impl.dart';
 export 'errors/auth_errors.dart';
+export 'inputs/inputs.dart';
 export 'mappers/user_mapper.dart';
 export 'repositories/auth_repository_impl.dart';
