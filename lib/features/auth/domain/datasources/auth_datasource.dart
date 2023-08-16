457ffa54c9ade4354afe3a90e079fa9728663a5f
Hector Montilla <elcama_86@msn.com>
2023-08-16 09:57:24 -0400

    auth datasource and repository created

---
+++ lib/features/auth/domain/datasources/auth_datasource.dart
@@ -0,0 +1,10 @@
+import '../entities/user.dart';
+
+abstract class AuthDatasource {
+  Stream<User> get user;
+  Future<void> signUp({required String email, required String password});
+  Future<void> logInWithGoogle();
+  Future<void> logInWithEmailAndPassword(
+      {required String email, required String password});
+  Future<void> logOut();
+}
