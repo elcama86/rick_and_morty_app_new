6dbae305edd260abd182a05920dc3a09434f4fc8
Hector Montilla <elcama_86@msn.com>
2023-08-17 10:24:27 -0400

    blocs removed from barrel file

--- lib/features/home/home.dart
+++ lib/features/home/home.dart
@@ -1,4 +1,3 @@
-export 'presentation/blocs/nav_drawer_cubit/nav_drawer_cubit.dart';
 export 'presentation/screens/home_screen.dart';
 export 'presentation/views/home_view.dart';
 export 'presentation/widgets/widgets.dart';
