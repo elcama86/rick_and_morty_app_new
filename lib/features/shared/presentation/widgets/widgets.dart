3b803ba6d383378cf5172a739b370c2cffabaae9
Hector Montilla <elcama_86@msn.com>
2023-08-17 10:25:09 -0400

    side menu and custom text form field added

--- lib/features/shared/presentation/widgets/widgets.dart
+++ lib/features/shared/presentation/widgets/widgets.dart
@@ -1,10 +1,12 @@
 export 'custom_bottom_navigation.dart';
 export 'custom_gradient.dart';
 export 'custom_message.dart';
+export 'custom_text_form_field.dart';
 export 'element_horizontal_listview.dart';
 export 'elements_by_entity.dart';
 export 'elements_mansory.dart';
 export 'elements_scroll_view.dart';
 export 'loading_spinner.dart';
 export 'scaffold_background.dart';
+export 'side_menu.dart';
 export 'title_sliver_app_bar.dart';
