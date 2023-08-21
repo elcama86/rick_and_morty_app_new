48b769f25aeb9a855bcb3c2ad01db158ce485c84
Hector Montilla <elcama_86@msn.com>
2023-08-21 15:49:21 -0400

    message loading according auth status

--- lib/features/auth/presentation/screens/check_auth_status_screen.dart
+++ lib/features/auth/presentation/screens/check_auth_status_screen.dart
@@ -1,21 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:rick_and_morty_app/features/shared/shared.dart';
+import 'package:rick_and_morty_app/features/auth/presentation/presentation.dart';
 
 class CheckAuthStatusScreen extends StatelessWidget {
-  const CheckAuthStatusScreen({super.key});
+  final AuthStatus status;
+
+  const CheckAuthStatusScreen({
+    super.key,
+    required this.status,
+  });
+
+  String message(AuthStatus status) {
+    switch (status) {
+      case AuthStatus.checking:
+        return 'Iniciando...';
+      case AuthStatus.loading:
+        return 'Cerrando sesión...';
+      default:
+        return 'Cargando...';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          LoadingSpinner(),
-          SizedBox(
-            height: 10.0,
-          ),
-          Text('Cargando...'),
-        ],
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircularProgressIndicator(
+              strokeWidth: 2.0,
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              message(status),
+            ),
+          ],
+        ),
       ),
     );
   }
