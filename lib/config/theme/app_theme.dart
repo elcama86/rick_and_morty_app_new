41063fa705bb9ccd930d27c9d728816a7e1d2147
Hector Montilla <elcama_86@msn.com>
2023-08-23 12:16:39 -0400

    titile text and filled button color props created

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -3,6 +3,8 @@ import 'package:google_fonts/google_fonts.dart';
 
 const colorSeed = Colors.white;
 final scaffoldBackgroundColor = Colors.grey.shade900;
+const titleTextColor = Colors.white;
+const filledButtonColor = Colors.black;
 
 class AppTheme {
   ThemeData getTheme() => ThemeData(
@@ -14,14 +16,16 @@ class AppTheme {
           titleLarge: GoogleFonts.amaticSc().copyWith(
             fontSize: 40.0,
             fontWeight: FontWeight.bold,
-            color: Colors.white,
+            color: titleTextColor,
           ),
           titleMedium: GoogleFonts.amaticSc().copyWith(
             fontSize: 30.0,
             fontWeight: FontWeight.bold,
+            color: titleTextColor,
           ),
           titleSmall: GoogleFonts.amaticSc().copyWith(
             fontSize: 20.0,
+            color: titleTextColor,
           ),
         ),
         appBarTheme: AppBarTheme(
@@ -29,6 +33,13 @@ class AppTheme {
           titleTextStyle: GoogleFonts.amaticSc().copyWith(
             fontSize: 25.0,
             fontWeight: FontWeight.bold,
+            color: titleTextColor,
+          ),
+        ),
+        filledButtonTheme: FilledButtonThemeData(
+          style: FilledButton.styleFrom(
+            backgroundColor: filledButtonColor,
+            disabledBackgroundColor: filledButtonColor.withOpacity(0.5),
           ),
         ),
       );
