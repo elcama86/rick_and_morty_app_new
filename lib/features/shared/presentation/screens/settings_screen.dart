fa5acf567f631def0d7bf182844c5dfa2e164b68
Hector Montilla <elcama_86@msn.com>
2023-08-24 15:29:53 -0400

    value and on changed switch list tile implemented

--- lib/features/shared/presentation/screens/settings_screen.dart
+++ lib/features/shared/presentation/screens/settings_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rick_and_morty_app/features/shared/shared.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
@@ -36,43 +38,63 @@ class _ThemeSettings extends StatelessWidget {
   Widget build(BuildContext context) {
     final textThemes = Theme.of(context).textTheme;
 
-    return Column(
-      children: [
-        ListTile(
-          leading: const Icon(Icons.palette_rounded),
-          title: Text(
-            'Tema establecido',
-            style: textThemes.titleMedium,
-          ),
-        ),
-        SwitchListTile(
-          title: Text(
-            'Tema claro',
-            style: textThemes.titleSmall,
-          ),
-          value: false,
-          onChanged: null,
-          secondary: const Icon(Icons.light_mode_outlined),
-        ),
-        SwitchListTile(
-          title: Text(
-            'Tema Oscuro',
-            style: textThemes.titleSmall,
-          ),
-          value: false,
-          onChanged: null,
-          secondary: const Icon(Icons.dark_mode_outlined),
-        ),
-        SwitchListTile(
-          title: Text(
-            'Tema predeterminado por sistema',
-            style: textThemes.titleSmall,
-          ),
-          value: false,
-          onChanged: null,
-          secondary: const Icon(Icons.brightness_medium_outlined),
-        ),
-      ],
+    return BlocBuilder<SettingsCubit, SettingsState>(
+      bloc: BlocProvider.of<SettingsCubit>(context),
+      builder: (context, state) {
+        final lightValue = state.themeMode == ThemeMode.light;
+        final darkValue = state.themeMode == ThemeMode.dark;
+        final systemValue = state.themeMode == ThemeMode.system;
+
+        return Column(
+          children: [
+            ListTile(
+              leading: const Icon(Icons.palette_rounded),
+              title: Text(
+                'Tema establecido',
+                style: textThemes.titleMedium,
+              ),
+            ),
+            SwitchListTile(
+              title: Text(
+                'Tema claro',
+                style: textThemes.titleSmall,
+              ),
+              value: lightValue,
+              onChanged: (_) =>
+                  context.read<SettingsCubit>().setTheme(ThemeMode.light),
+              secondary: Icon(
+                lightValue ? Icons.light_mode : Icons.light_mode_outlined,
+              ),
+            ),
+            SwitchListTile(
+              title: Text(
+                'Tema Oscuro',
+                style: textThemes.titleSmall,
+              ),
+              value: darkValue,
+              onChanged: (_) =>
+                  context.read<SettingsCubit>().setTheme(ThemeMode.dark),
+              secondary: Icon(
+                darkValue ? Icons.dark_mode : Icons.dark_mode_outlined,
+              ),
+            ),
+            SwitchListTile(
+              title: Text(
+                'Tema predeterminado por sistema',
+                style: textThemes.titleSmall,
+              ),
+              value: systemValue,
+              onChanged: (_) =>
+                  context.read<SettingsCubit>().setTheme(ThemeMode.system),
+              secondary: Icon(
+                systemValue
+                    ? Icons.brightness_medium
+                    : Icons.brightness_medium_outlined,
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
