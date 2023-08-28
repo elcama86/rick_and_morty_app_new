4704765fd0a579e0bfb0932c8cd10c4eaa5528e1
Hector Montilla <elcama_86@msn.com>
2023-08-28 14:57:58 -0400

    get theme by prefs and key value service impl

--- lib/features/shared/presentation/blocs/settings_cubit/settings_cubit.dart
+++ lib/features/shared/presentation/blocs/settings_cubit/settings_cubit.dart
@@ -1,13 +1,38 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+import 'package:rick_and_morty_app/features/shared/infrastructure/services/key_value_storage_service.dart';
 
 part 'settings_state.dart';
 
 class SettingsCubit extends Cubit<SettingsState> {
-  SettingsCubit() : super(const SettingsState());
+  final KeyValueStorageService keyValueStorageService;
 
-  void setTheme(ThemeMode themeMode) {
+  SettingsCubit({
+    required this.keyValueStorageService,
+  }) : super(const SettingsState()) {
+    getThemeByPrefs();
+  }
+
+  void getThemeByPrefs() {
+    String? themeSelected = keyValueStorageService.getValue<String>('theme');
+
+    if (themeSelected != null) {
+      final themeMode = ThemeMode.values.byName(themeSelected);
+
+      emit(
+        state.copyWith(
+          themeMode: themeMode,
+        ),
+      );
+    }
+  }
+
+  void setTheme(ThemeMode themeMode) async {
+    if (state.themeMode == themeMode) return;
+
+    await keyValueStorageService.setKeyValue('theme', themeMode.name);
+
     emit(
       state.copyWith(
         themeMode: themeMode,
