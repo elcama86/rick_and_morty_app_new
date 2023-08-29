b67c2c1881969388a94fdfe6dab6d4be797bd7cb
Hector Montilla <elcama_86@msn.com>
2023-08-29 14:31:14 -0400

    language option property added

--- lib/features/shared/presentation/blocs/settings_cubit/settings_state.dart
+++ lib/features/shared/presentation/blocs/settings_cubit/settings_state.dart
@@ -1,19 +1,28 @@
 part of 'settings_cubit.dart';
 
+enum LanguageOption {
+  spanish,
+  english,
+}
+
 class SettingsState extends Equatable {
   final ThemeMode themeMode;
+  final LanguageOption language;
 
   const SettingsState({
     this.themeMode = ThemeMode.system,
+    this.language = LanguageOption.spanish,
   });
 
   SettingsState copyWith({
     ThemeMode? themeMode,
+    LanguageOption? language,
   }) =>
       SettingsState(
         themeMode: themeMode ?? this.themeMode,
+        language: language ?? this.language,
       );
 
   @override
-  List<Object> get props => [themeMode];
+  List<Object> get props => [themeMode, language];
 }
