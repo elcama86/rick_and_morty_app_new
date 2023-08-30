0ffd92f39dddbbf1252f70c78090014a1539ab18
Hector Montilla <elcama_86@msn.com>
2023-08-30 11:26:01 -0400

    app localizations class created and file added

---
+++ lib/config/localizations/app_localizations.dart
@@ -0,0 +1,75 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+class AppLocalizations {
+  late final Locale _locale;
+  late Map<String, String> _localizedValues;
+
+  AppLocalizations(this._locale);
+
+  static const Iterable<Locale> supportedLocales = [
+    Locale('es'),
+    Locale('en'),
+  ];
+
+  static const Iterable<LocalizationsDelegate<dynamic>> localizationsDelegates =
+      [
+    AppLocalizations.delegate,
+    GlobalMaterialLocalizations.delegate,
+    GlobalWidgetsLocalizations.delegate,
+    GlobalCupertinoLocalizations.delegate,
+  ];
+
+  static Locale? localeResolutionCallback(
+      Locale? deviceLocale, Iterable<Locale> supportedLocales) {
+    for (var locale in supportedLocales) {
+      if (locale.languageCode == deviceLocale!.languageCode) {
+        return deviceLocale;
+      }
+    }
+    return supportedLocales.first;
+  }
+
+  static AppLocalizations of(BuildContext context) {
+    return Localizations.of<AppLocalizations>(context, AppLocalizations)!;
+  }
+
+  Future loadLanguage() async {
+    String jsonStringValues =
+        await rootBundle.loadString('assets/i18n/${_locale.languageCode}.json');
+    Map<String, dynamic> mappedValues = json.decode(jsonStringValues);
+
+    _localizedValues =
+        mappedValues.map((key, value) => MapEntry(key, value.toString()));
+  }
+
+  String? translate(String key) {
+    return _localizedValues[key];
+  }
+
+  static const LocalizationsDelegate<AppLocalizations> delegate =
+      _AppLocalizationsDelegate();
+}
+
+class _AppLocalizationsDelegate
+    extends LocalizationsDelegate<AppLocalizations> {
+  const _AppLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) {
+    return ['es', 'en'].contains(locale.languageCode);
+  }
+
+  @override
+  Future<AppLocalizations> load(Locale locale) async {
+    AppLocalizations appLocalizations = AppLocalizations(locale);
+    await appLocalizations.loadLanguage();
+    return appLocalizations;
+  }
+
+  @override
+  bool shouldReload(_AppLocalizationsDelegate old) => false;
+}
