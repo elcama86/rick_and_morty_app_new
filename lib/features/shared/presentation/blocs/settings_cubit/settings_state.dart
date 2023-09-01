2d8af35f9b63e91e5d5fdfb9760a25f9cfeeedcb
Hector Montilla <elcama_86@msn.com>
2023-09-01 15:23:24 -0400

    is system language prop added

--- lib/features/shared/presentation/blocs/settings_cubit/settings_state.dart
+++ lib/features/shared/presentation/blocs/settings_cubit/settings_state.dart
@@ -3,21 +3,25 @@ part of 'settings_cubit.dart';
 class SettingsState extends Equatable {
   final ThemeMode themeMode;
   final String language;
+  final bool isSystemLanguage;
 
   const SettingsState({
     this.themeMode = ThemeMode.system,
-    this.language = 'es',
+    this.language = 'en',
+    this.isSystemLanguage = true,
   });
 
   SettingsState copyWith({
     ThemeMode? themeMode,
     String? language,
+    bool? isSystemLanguage,
   }) =>
       SettingsState(
         themeMode: themeMode ?? this.themeMode,
         language: language ?? this.language,
+        isSystemLanguage: isSystemLanguage ?? this.isSystemLanguage,
       );
 
   @override
-  List<Object> get props => [themeMode, language];
+  List<Object> get props => [themeMode, language, isSystemLanguage];
 }
