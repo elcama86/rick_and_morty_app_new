d6d55f434211119d66e28f428b89556452bd1d78
Hector Montilla <elcama_86@msn.com>
2023-09-01 15:24:30 -0400

    stream subscription for language changes

--- lib/features/shared/presentation/blocs/settings_cubit/settings_cubit.dart
+++ lib/features/shared/presentation/blocs/settings_cubit/settings_cubit.dart
@@ -1,23 +1,29 @@
+import 'dart:async';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+import 'package:rick_and_morty_app/config/config.dart';
 import 'package:rick_and_morty_app/features/shared/shared.dart';
 
 part 'settings_state.dart';
 
 class SettingsCubit extends Cubit<SettingsState> {
   final KeyValueStorageService keyValueStorageService;
+  late final StreamSubscription<String> _languageSubscription;
 
   SettingsCubit({
     required this.keyValueStorageService,
   }) : super(const SettingsState()) {
-    getSettingsByPrefs();
+    setSettingsByPrefs();
   }
 
-  void getSettingsByPrefs() {
+  void setSettingsByPrefs() {
     String? selectedTheme = keyValueStorageService.getValue<String>('theme');
     String? selectedLanguage =
         keyValueStorageService.getValue<String>('language');
+    bool? isSystemLanguage =
+        keyValueStorageService.getValue<bool>('isSystemLanguage');
 
     emit(
       state.copyWith(
@@ -25,8 +31,19 @@ class SettingsCubit extends Cubit<SettingsState> {
             ? ThemeMode.values.byName(selectedTheme)
             : state.themeMode,
         language: selectedLanguage ?? state.language,
+        isSystemLanguage: isSystemLanguage ?? state.isSystemLanguage,
       ),
     );
+
+    _languageSubscription = AppLocalizations.language.stream.listen((language) {
+      if (state.isSystemLanguage) {
+        emit(
+          state.copyWith(
+            language: language,
+          ),
+        );
+      }
+    });
   }
 
   void setTheme(ThemeMode themeMode) async {
@@ -42,16 +59,34 @@ class SettingsCubit extends Cubit<SettingsState> {
   }
 
   void setLanguage(String language) async {
-    if (state.language == language) return;
+    if (state.language == language && !state.isSystemLanguage) return;
 
     await keyValueStorageService.setKeyValue('language', language);
-
-    Locale(language);
+    await keyValueStorageService.setKeyValue('isSystemLanguage', false);
 
     emit(
       state.copyWith(
         language: language,
+        isSystemLanguage: false,
       ),
     );
+  }
+
+  void setSystemLanguage(String language) async {
+    if (language == 'system_value') return;
+
+    await keyValueStorageService.setKeyValue('isSystemLanguage', true);
+
+    emit(
+      state.copyWith(
+        isSystemLanguage: true,
+      ),
+    );
+  }
+
+  @override
+  Future<void> close() {
+    _languageSubscription.cancel();
+    return super.close();
   }
 }
