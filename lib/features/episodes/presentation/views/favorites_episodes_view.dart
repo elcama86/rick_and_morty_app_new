f7ef01012de139ccf586882c293f597c06481d0a
Hector Montilla <elcama_86@msn.com>
2023-09-01 16:43:15 -0400

    initial loading only and labels translation added

--- lib/features/episodes/presentation/views/favorites_episodes_view.dart
+++ lib/features/episodes/presentation/views/favorites_episodes_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:rick_and_morty_app/config/config.dart';
 import 'package:rick_and_morty_app/features/episodes/episodes.dart';
 import 'package:rick_and_morty_app/features/shared/shared.dart';
 
@@ -52,38 +53,78 @@ class _FavoritesEpisodesViewState extends State<FavoritesEpisodesView> {
 
   @override
   Widget build(BuildContext context) {
+    return BlocBuilder<FavoritesEpisodesBloc, FavoritesEpisodesState>(
+      builder: (context, state) {
+        final List<Episode> favoritesEpisodes =
+            state.favoritesEpisodes.values.toList();
+
+        if (isLoading && favoritesEpisodes.isEmpty && state.isFirstLoad) {
+          return const _LoadingFavorites();
+        }
+
+        if (favoritesEpisodes.isEmpty) {
+          isLastPage = false;
+          return const _NoFavorites();
+        }
+
+        return ElementsScrollView(
+          controller: controller,
+          elements: favoritesEpisodes,
+          title: "favorites",
+          leading: IconButton(
+            onPressed: () => context.pop(),
+            icon: const Icon(Icons.arrow_back),
+          ),
+          loadNextPage: loadNextFavorites,
+          showBottomNavBar: context.read<BottomNavBarCubit>().show,
+          hideBottomNavBar: context.read<BottomNavBarCubit>().hide,
+          setScrollPositions:
+              context.read<BottomNavBarCubit>().setScrollPositions,
+        );
+      },
+    );
+  }
+}
+
+class _NoFavorites extends StatelessWidget {
+  const _NoFavorites();
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocBuilder<FavoritesEpisodesBloc, FavoritesEpisodesState>(
-        builder: (context, state) {
-          final List<Episode> favoritesEpisodes =
-              state.favoritesEpisodes.values.toList();
+      appBar: AppBar(
+        title: Text(
+          AppLocalizations.of(context).translate('favorites'),
+        ),
+        leading: IconButton(
+          onPressed: () => context.pop(),
+          icon: const Icon(Icons.arrow_back),
+        ),
+      ),
+      body: const CustomMessage(
+        message: "no_favorites_loaded",
+      ),
+    );
+  }
+}
 
-          if (favoritesEpisodes.isEmpty) {
-            if (isLoading) {
-              return const LoadingSpinner(
-                message: 'Cargando favoritos...',
-              );
-            }
-            return const CustomMessage(
-              message: "No existen episodios favoritos seleccionados",
-            );
-          }
+class _LoadingFavorites extends StatelessWidget {
+  const _LoadingFavorites();
 
-          return ElementsScrollView(
-            controller: controller,
-            elements: favoritesEpisodes,
-            title: "Favoritos",
-            leading: IconButton(
-              onPressed: () => context.pop(),
-              icon: const Icon(Icons.arrow_back),
-            ),
-            loadNextPage: loadNextFavorites,
-            showBottomNavBar: context.read<BottomNavBarCubit>().show,
-            hideBottomNavBar: context.read<BottomNavBarCubit>().hide,
-            setScrollPositions:
-                context.read<BottomNavBarCubit>().setScrollPositions,
-          );
-        },
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          AppLocalizations.of(context).translate('favorites'),
+        ),
+        leading: IconButton(
+          onPressed: () => context.pop(),
+          icon: const Icon(Icons.arrow_back),
+        ),
+      ),
+      body: const LoadingSpinner(
+        message: 'loading_favorites',
       ),
     );
   }
