e0ab27c26302b09f377f583be518bca736d079ac
Hector Montilla <elcama_86@msn.com>
2023-09-06 11:08:37 -0400

    set query loading event implemented

--- lib/features/episodes/presentation/blocs/search_episodes_bloc/search_episodes_bloc.dart
+++ lib/features/episodes/presentation/blocs/search_episodes_bloc/search_episodes_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:rick_and_morty_app/features/episodes/episodes.dart';
@@ -16,6 +18,7 @@ class SearchEpisodesBloc
     on<SetQueryValue>(_setQueryValue);
     on<SetQueryResults>(_setQueryResults);
     on<SetQueryErrorMessage>(_setQueryErrorMessage);
+    on<SetQueryLoading>(_setQueryLoading);
   }
 
   void _setQueryValue(SetQueryValue event, Emitter<SearchEpisodesState> emit) {
@@ -44,28 +47,43 @@ class SearchEpisodesBloc
     );
   }
 
+  void _setQueryLoading(
+      SetQueryLoading event, Emitter<SearchEpisodesState> emit) {
+    emit(
+      state.copyWith(
+        isLoading: event.value,
+      ),
+    );
+  }
+
   Future<List<Episode>> searchEpisodesByQuery(String query) async {
     try {
+      if (query != state.query) add(SetQueryLoading(true));
+
       add(SetQueryValue(query));
 
       final List<Episode> results = await searchEpisodes(query);
 
       add(SetQueryResults(results));
+      add(SetQueryLoading(false));
       add(SetQueryErrorMessage(''));
 
       return results;
     } on EpisodeNotFound {
       add(SetQueryResults([]));
+      add(SetQueryLoading(false));
       add(SetQueryErrorMessage("no_episodes_by_name,$query"));
 
       return [];
     } on CustomError catch (e) {
       add(SetQueryResults([]));
+      add(SetQueryLoading(false));
       add(SetQueryErrorMessage("error_search_result,${e.message}"));
 
       return [];
     } catch (e) {
       add(SetQueryResults([]));
+      add(SetQueryLoading(false));
       add(SetQueryErrorMessage("unexpected_search_error"));
 
       return [];
