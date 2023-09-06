f84b33cbe06493b8dcedfbde47f04c16f87c47e7
Hector Montilla <elcama_86@msn.com>
2023-09-06 11:08:07 -0400

    is loading prop added

--- lib/features/episodes/presentation/blocs/search_episodes_bloc/search_episodes_state.dart
+++ lib/features/episodes/presentation/blocs/search_episodes_bloc/search_episodes_state.dart
@@ -4,24 +4,28 @@ class SearchEpisodesState extends Equatable {
   final String query;
   final List<Episode> results;
   final String errorMessage;
+  final bool isLoading;
 
   const SearchEpisodesState({
     this.query = '',
     this.results = const [],
     this.errorMessage = '',
+    this.isLoading = false,
   });
 
   SearchEpisodesState copyWith({
     String? query,
     List<Episode>? results,
     String? errorMessage,
+    bool? isLoading,
   }) =>
       SearchEpisodesState(
         query: query ?? this.query,
         results: results ?? this.results,
         errorMessage: errorMessage ?? this.errorMessage,
+        isLoading: isLoading ?? this.isLoading,
       );
 
   @override
-  List<Object> get props => [query, results, errorMessage];
+  List<Object> get props => [query, results, errorMessage, isLoading];
 }
