d62bc9cdeda2463f6ecbde04ed01b470aca0021a
Hector Montilla <elcama_86@msn.com>
2023-09-08 16:01:43 -0400

    scroll controlers and double positions props added

--- lib/features/shared/presentation/blocs/bottom_nav_bar_cubit/bottom_nav_bar_state.dart
+++ lib/features/shared/presentation/blocs/bottom_nav_bar_cubit/bottom_nav_bar_state.dart
@@ -1,28 +1,77 @@
 part of 'bottom_nav_bar_cubit.dart';
 
-class BottomNavBarState {
+class BottomNavBarState extends Equatable {
   final int currentIndex;
   final bool isVisible;
   final double appBarHeight;
-  final Map<int, double> scrollPositions;
+  final ScrollController episodesController;
+  final ScrollController favoritesController;
+  final double episodesPosition;
+  final double favoritesPosition;
 
   const BottomNavBarState({
     this.currentIndex = 0,
     this.isVisible = true,
     this.appBarHeight = kToolbarHeight,
-    this.scrollPositions = const {},
+    required this.episodesController,
+    required this.favoritesController,
+    this.episodesPosition = 0.0,
+    this.favoritesPosition = 0.0,
   });
 
   BottomNavBarState copyWith({
     int? currentIndex,
     bool? isVisible,
     double? appBarHeight,
-    Map<int, double>? scrollPositions,
+    ScrollController? episodesController,
+    ScrollController? favoritesController,
+    double? episodesPosition,
+    double? favoritesPosition,
   }) =>
       BottomNavBarState(
         currentIndex: currentIndex ?? this.currentIndex,
         isVisible: isVisible ?? this.isVisible,
         appBarHeight: appBarHeight ?? this.appBarHeight,
-        scrollPositions: scrollPositions ?? this.scrollPositions,
+        episodesController: episodesController ?? this.episodesController,
+        favoritesController: favoritesController ?? this.favoritesController,
+        episodesPosition: episodesPosition ?? this.episodesPosition,
+        favoritesPosition: favoritesPosition ?? this.favoritesPosition,
       );
+
+  double get finalPosition {
+    switch (currentIndex) {
+      case 0:
+        return episodesController.hasClients
+            ? episodesController.position.pixels
+            : 0.0;
+      case 1:
+        return favoritesController.hasClients
+            ? favoritesController.position.pixels
+            : 0.0;
+      default:
+        return 0.0;
+    }
+  }
+
+  double get currentPosition {
+    switch (currentIndex) {
+      case 0:
+        return episodesPosition;
+      case 1:
+        return favoritesPosition;
+      default:
+        return 0.0;
+    }
+  }
+
+  @override
+  List<Object> get props => [
+        currentIndex,
+        isVisible,
+        appBarHeight,
+        episodesController,
+        favoritesController,
+        episodesPosition,
+        favoritesPosition,
+      ];
 }
