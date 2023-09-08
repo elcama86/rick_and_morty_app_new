26b156fb4899a357e7462702924f4924c154a351
Hector Montilla <elcama_86@msn.com>
2023-09-08 16:03:59 -0400

    bloc builder with bottom nav bar cubit implemented

--- lib/features/shared/presentation/widgets/custom_bottom_navigation.dart
+++ lib/features/shared/presentation/widgets/custom_bottom_navigation.dart
@@ -5,7 +5,7 @@ import 'package:rick_and_morty_app/features/shared/shared.dart';
 
 class CustomBottomNavigation extends StatelessWidget {
   final int index;
-  final void Function (int, BuildContext) onItemTapped;
+  final void Function(int, BuildContext) onItemTapped;
 
   const CustomBottomNavigation({
     super.key,
@@ -18,47 +18,49 @@ class CustomBottomNavigation extends StatelessWidget {
     final colors = Theme.of(context).colorScheme;
     final scaffoldBackgroundColor = Theme.of(context).scaffoldBackgroundColor;
 
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 400),
-      height: context.select((BottomNavBarCubit bottomNavBarCubit) =>
-          bottomNavBarCubit.state.isVisible ? kBottomNavigationBarHeight : 0.0),
-      child: Wrap(
-        children: [
-          BottomNavigationBar(
-            elevation: 16.0,
-            currentIndex: index,
-            onTap: (value) => onItemTapped(value, context),
-            selectedItemColor: colors.primary,
-            showUnselectedLabels: true,
-            unselectedItemColor: colors.secondary,
-            backgroundColor: context.select(
-              (BottomNavBarCubit bottomNavBarCubit) {
-                final position =
-                    bottomNavBarCubit.state.scrollPositions[index] ?? 0.0;
-                final appBarHeight = bottomNavBarCubit.state.appBarHeight;
+    return BlocBuilder<BottomNavBarCubit, BottomNavBarState>(
+      builder: (context, state) {
+        final position = state.currentPosition;
+        final appBarHeight = state.appBarHeight;
 
-                return position > appBarHeight
+        if (state.finalPosition == 0.0) {
+          context.read<BottomNavBarCubit>().showAfterRemoveFavorite();
+        }
+
+        return AnimatedContainer(
+          duration: const Duration(milliseconds: 400),
+          height: state.isVisible ? kBottomNavigationBarHeight : 0.0,
+          child: Wrap(
+            children: [
+              BottomNavigationBar(
+                elevation: 16.0,
+                currentIndex: index,
+                onTap: (value) => onItemTapped(value, context),
+                selectedItemColor: colors.primary,
+                showUnselectedLabels: true,
+                unselectedItemColor: colors.secondary,
+                backgroundColor: position > appBarHeight
                     ? colors.onInverseSurface
-                    : scaffoldBackgroundColor;
-              },
-            ),
-            items: [
-              BottomNavigationBarItem(
-                icon: Icon(
-                  index == 0 ? Icons.home : Icons.home_outlined,
-                ),
-                label: AppLocalizations.of(context).translate('home'),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(
-                  index == 1 ? Icons.favorite : Icons.favorite_outline,
-                ),
-                label: AppLocalizations.of(context).translate('favorites'),
+                    : scaffoldBackgroundColor,
+                items: [
+                  BottomNavigationBarItem(
+                    icon: Icon(
+                      index == 0 ? Icons.home : Icons.home_outlined,
+                    ),
+                    label: AppLocalizations.of(context).translate('home'),
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(
+                      index == 1 ? Icons.favorite : Icons.favorite_outline,
+                    ),
+                    label: AppLocalizations.of(context).translate('favorites'),
+                  ),
+                ],
               ),
             ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
