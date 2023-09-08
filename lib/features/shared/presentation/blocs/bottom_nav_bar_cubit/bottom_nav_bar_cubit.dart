3910fb87010583cf431bce84c3b46313fca9f181
Hector Montilla <elcama_86@msn.com>
2023-09-08 16:03:19 -0400

    show after remove added and set position modified

--- lib/features/shared/presentation/blocs/bottom_nav_bar_cubit/bottom_nav_bar_cubit.dart
+++ lib/features/shared/presentation/blocs/bottom_nav_bar_cubit/bottom_nav_bar_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -5,7 +6,13 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 part 'bottom_nav_bar_state.dart';
 
 class BottomNavBarCubit extends Cubit<BottomNavBarState> {
-  BottomNavBarCubit() : super(const BottomNavBarState());
+  BottomNavBarCubit()
+      : super(
+          BottomNavBarState(
+            episodesController: ScrollController(),
+            favoritesController: ScrollController(),
+          ),
+        );
 
   void show() {
     if (!state.isVisible) {
@@ -17,8 +24,30 @@ class BottomNavBarCubit extends Cubit<BottomNavBarState> {
     }
   }
 
+  void showAfterRemoveFavorite() {
+    if (state.currentIndex != 1 || state.isVisible) return;
+
+    emit(
+      state.copyWith(
+        isVisible: true,
+        favoritesPosition: 0.0,
+      ),
+    );
+  }
+
+  double getCurrentPosition() {
+    switch (state.currentIndex) {
+      case 0:
+        return state.episodesPosition;
+      case 1:
+        return state.favoritesPosition;
+      default:
+        return 0.0;
+    }
+  }
+
   void hide() {
-    double position = state.scrollPositions[state.currentIndex] ?? 0.0;
+    double position = getCurrentPosition();
     if (position < state.appBarHeight) return;
     if (state.isVisible) {
       emit(
@@ -29,43 +58,41 @@ class BottomNavBarCubit extends Cubit<BottomNavBarState> {
     }
   }
 
-  void checkScrollPosition(double position, ScrollDirection direction) {
-    if (direction == ScrollDirection.forward) {
-      if (position < state.appBarHeight) {
-        if (position == 0.0) {
-          state.scrollPositions.update(state.currentIndex, (value) => position);
-        }
-      } else {
-        state.scrollPositions.update(state.currentIndex, (value) => position);
-      }
-    } else {
-      if (position > state.appBarHeight) {
-        state.scrollPositions.update(state.currentIndex, (value) => position);
-      }
+  void setCurrentPosition(double position) {
+    switch (state.currentIndex) {
+      case 0:
+        emit(
+          state.copyWith(
+            episodesPosition: position,
+          ),
+        );
+        break;
+      case 1:
+        emit(
+          state.copyWith(
+            favoritesPosition: position,
+          ),
+        );
+        break;
+      default:
+        throw UnimplementedError(
+            'No existe la implementación para este index ${state.currentIndex}');
     }
   }
 
-  void setScrollPositions(double position, ScrollDirection direction) {
-    if (state.scrollPositions[state.currentIndex] != null) {
-      checkScrollPosition(position, direction);
-
-      emit(
-        state.copyWith(
-          scrollPositions: {...state.scrollPositions},
-        ),
-      );
-
-      return;
+  void setScrollPosition(double position, ScrollDirection direction) {
+    if (direction == ScrollDirection.forward ||
+        direction == ScrollDirection.idle) {
+      if (position < state.appBarHeight) {
+        if (position == 0.0) {
+          setCurrentPosition(position);
+        }
+      } else {
+        setCurrentPosition(position);
+      }
+    } else {
+      if (position > state.appBarHeight) setCurrentPosition(position);
     }
-
-    emit(
-      state.copyWith(
-        scrollPositions: {
-          ...state.scrollPositions,
-          state.currentIndex: position
-        },
-      ),
-    );
   }
 
   void setCurrentIndex(int index) {
