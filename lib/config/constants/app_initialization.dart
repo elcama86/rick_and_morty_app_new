f1721df96a497ad750367ea1f3a57ad790216785
Hector Montilla <elcama_86@msn.com>
2023-09-14 11:40:58 -0400

    app initialization created

---
+++ lib/config/constants/app_initialization.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:rick_and_morty_app/features/auth/auth.dart';
+import 'package:rick_and_morty_app/features/shared/infrastructure/services/key_value_storage_service_impl.dart';
+
+import '../helpers/human_formats.dart';
+import 'constants.dart';
+
+class AppInitialization {
+  static Future<void> initialize() async {
+    FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+    await Certificate.register();
+    await OrientationSetting.setDefaultOrientation();
+    await Environment.initEnvironment();
+    await removeSplash();
+    await AuthBloc.initializeServices();
+    await KeyValueStorageServiceImpl.init();
+    HumanFormats.initializeDates();
+  }
+
+  static Future<void> removeSplash() async {
+    Future.delayed(
+        const Duration(seconds: 3), () => FlutterNativeSplash.remove());
+  }
+
+  static WidgetsBinding widgetsBinding =
+      WidgetsFlutterBinding.ensureInitialized();
+}
