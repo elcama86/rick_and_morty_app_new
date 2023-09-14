371478683a972ddcf84da5ec39350220e0e738d9
Hector Montilla <elcama_86@msn.com>
2023-09-14 11:41:34 -0400

    widgets flutter binding replaced

--- lib/config/constants/certificate.dart
+++ lib/config/constants/certificate.dart
@@ -1,11 +1,11 @@
 import 'dart:io';
-
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'app_initialization.dart';
+
 class Certificate {
-  static register() async {
-    WidgetsFlutterBinding.ensureInitialized();
+  static Future<void> register() async {
+    AppInitialization.widgetsBinding;
     ByteData data =
         await PlatformAssetBundle().load('assets/certificate/certificate.pem');
     SecurityContext.defaultContext
