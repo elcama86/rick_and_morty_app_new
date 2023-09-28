b2b21c3cc94064021a40c5df3d4bba82380394b3
Hector Montilla <elcama_86@msn.com>
2023-09-28 13:13:16 -0400

    favorites settings option created

--- lib/features/shared/presentation/screens/settings_screen.dart
+++ lib/features/shared/presentation/screens/settings_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rick_and_morty_app/config/config.dart';
+import 'package:rick_and_morty_app/features/episodes/episodes.dart';
 import 'package:rick_and_morty_app/features/shared/shared.dart';
 
 class SettingsScreen extends StatelessWidget {
@@ -31,6 +32,8 @@ class _SettingsView extends StatelessWidget {
         _ThemeSettings(),
         Divider(),
         _LanguageSettings(),
+        Divider(),
+        _FavoritesSettings(),
       ],
     );
   }
@@ -154,6 +157,74 @@ class _ThemeSettings extends StatelessWidget {
                     ? Icons.brightness_medium
                     : Icons.brightness_medium_outlined,
               ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class _FavoritesSettings extends StatelessWidget {
+  const _FavoritesSettings();
+
+  @override
+  Widget build(BuildContext context) {
+    final textThemes = Theme.of(context).textTheme;
+    final colors = Theme.of(context).colorScheme;
+
+    return BlocBuilder<FavoritesEpisodesBloc, FavoritesEpisodesState>(
+      builder: (context, state) {
+        return Column(
+          children: [
+            ListTile(
+              leading: const Icon(Icons.favorite),
+              title: Text(
+                AppLocalizations.of(context).translate('favorites'),
+                style: textThemes.titleMedium,
+              ),
+            ),
+            ListTile(
+              contentPadding: const EdgeInsets.only(left: 24.0, right: 40.0),
+              title: Text(
+                AppLocalizations.of(context).translate('selected_favorites'),
+                style: textThemes.titleSmall,
+              ),
+              trailing: Text(
+                '${state.totalFavorites}',
+                style: textThemes.titleSmall?.copyWith(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            TextButton.icon(
+              style: TextButton.styleFrom(
+                foregroundColor: colors.error,
+                textStyle: textThemes.titleSmall,
+              ),
+              onPressed: state.totalFavorites > 0
+                  ? () => SharedUtils.showAlertDialog(
+                        context: context,
+                        title: 'attention',
+                        message: 'confirm_delete_favorites_message',
+                        acceptAction: context
+                            .read<FavoritesEpisodesBloc>()
+                            .clearEpisodesDb,
+                        iconColor: colors.error,
+                        acceptButtonColor: colors.error,
+                      )
+                  : null,
+              icon: const Icon(Icons.delete_forever),
+              label: Text(
+                AppLocalizations.of(context).translate('delete_favorites'),
+               
+              ),
+            ),
+            const SizedBox(
+              height: 20.0,
             ),
           ],
         );
