f5637604617dc6838de1f688155b552efd9eebf2
Hector Montilla <elcama_86@msn.com>
2023-10-18 13:30:44 -0400

    episode poster contain and remove function created

--- lib/features/episodes/presentation/widgets/episode_poster.dart
+++ lib/features/episodes/presentation/widgets/episode_poster.dart
@@ -1,10 +1,12 @@
 import 'dart:math';
 
-import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:animate_do/animate_do.dart';
 import 'package:rick_and_morty_app/config/config.dart';
 import 'package:rick_and_morty_app/features/episodes/episodes.dart';
+import 'package:rick_and_morty_app/features/shared/shared.dart';
 
 class EpisodePoster extends StatelessWidget {
   final Episode episode;
@@ -16,33 +18,108 @@ class EpisodePoster extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final textThemes = Theme.of(context).textTheme;
     final random = Random();
 
     return FadeInUp(
       from: random.nextInt(100) + 50,
       delay: Duration(milliseconds: random.nextInt(300)),
-      child: GestureDetector(
-        onTap: () => context.push('/episodes/episode/${episode.id}'),
-        child: Column(
-          children: [
-            _EpisodeCard(
-              episode: episode,
-            ),
-            Text(
-              episode.name,
-              textAlign: TextAlign.center,
-              style: textThemes.titleSmall,
-            ),
-          ],
-        ),
+      child: _EpisodePosterContain(
+        episode: episode,
       ),
     );
   }
 }
 
-class _EpisodeCard extends StatelessWidget {
-  const _EpisodeCard({
+class _EpisodePosterContain extends StatefulWidget {
+  final Episode episode;
+
+  const _EpisodePosterContain({
+    required this.episode,
+  });
+
+  @override
+  State<_EpisodePosterContain> createState() => _EpisodePosterContainState();
+}
+
+class _EpisodePosterContainState extends State<_EpisodePosterContain> {
+  late AnimationController controller;
+
+  void _removeFavorite() {
+    controller.reset();
+    controller.forward().then(
+          (_) => context
+              .read<FavoritesEpisodesBloc>()
+              .toggleFavorite(widget.episode)
+              .then((_) {
+            final totalFavorites =
+                context.read<FavoritesEpisodesBloc>().state.totalFavorites;
+
+            context
+                .read<BottomNavBarCubit>()
+                .updateFavoritesPosition(totalFavorites);
+
+            SharedUtils.showSnackbar(context, 'episode,${widget.episode.id},episode_deleted');
+          }),
+        );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<BottomNavBarCubit, BottomNavBarState>(
+      builder: (context, state) {
+        final currentIndex = state.currentIndex;
+
+        return GestureDetector(
+          onTap: () => context.push('/episodes/episode/${widget.episode.id}'),
+          onLongPress: currentIndex == 1 ? _removeFavorite : null,
+          child: currentIndex == 1
+              ? ShakeX(
+                  manualTrigger: true,
+                  controller: (animationCtrl) => controller = animationCtrl,
+                  duration: const Duration(milliseconds: 500),
+                  child: _EpisodePosterColumn(
+                    key: ValueKey<int>(widget.episode.id),
+                    episode: widget.episode,
+                  ),
+                )
+              : _EpisodePosterColumn(
+                  episode: widget.episode,
+                ),
+        );
+      },
+    );
+  }
+}
+
+class _EpisodePosterColumn extends StatelessWidget {
+  final Episode episode;
+
+  const _EpisodePosterColumn({
+    super.key,
+    required this.episode,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final textThemes = Theme.of(context).textTheme;
+
+    return Column(
+      children: [
+        _EpisodePosterCard(
+          episode: episode,
+        ),
+        Text(
+          episode.name,
+          textAlign: TextAlign.center,
+          style: textThemes.titleSmall,
+        ),
+      ],
+    );
+  }
+}
+
+class _EpisodePosterCard extends StatelessWidget {
+  const _EpisodePosterCard({
     required this.episode,
   });
 
