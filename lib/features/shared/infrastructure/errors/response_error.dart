e5798da8721fbee56dcdc7aa417725a0b42017df
Hector Montilla <elcama_86@msn.com>
2023-10-20 13:23:59 -0400

    location not found error added

--- lib/features/shared/infrastructure/errors/response_error.dart
+++ lib/features/shared/infrastructure/errors/response_error.dart
@@ -2,6 +2,8 @@ class EpisodeNotFound implements Exception {}
 
 class CharacterNotFound implements Exception {}
 
+class LocationNotFound implements Exception {}
+
 class CustomError implements Exception {
   final String message;
 
