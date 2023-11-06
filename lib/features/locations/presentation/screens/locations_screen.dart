724b3fd95ef3e0eca92e1879a49c6711166af12f
Hector Montilla <elcama_86@msn.com>
2023-11-06 11:22:45 -0400

    locations screen created

---
+++ lib/features/locations/presentation/screens/locations_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rick_and_morty_app/features/locations/locations.dart';
+import 'package:rick_and_morty_app/features/shared/shared.dart';
+
+class LocationsScreen extends StatelessWidget {
+  const LocationsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<LocationsBloc, LocationsState>(
+      listener: (context, state) {
+        if (state.errorMessage.isNotEmpty && !state.isLoading) {
+          SharedUtils.showSnackbar(context, state.errorMessage);
+        }
+      },
+      child: BlocBuilder<LocationsBloc, LocationsState>(
+        builder: (context, state) {
+          final locations = state.locations;
+          final isLoading = state.isLoading;
+          final errorLoading = state.errorMessage.isNotEmpty;
+
+          return Scaffold(
+            appBar: SharedUtils.appBarContain(locations, 'locations', context),
+            body: ScaffoldBodyList(
+              elements: locations,
+              isLoading: isLoading,
+              loadingMessage: 'loading_locations',
+              emptyMessage: 'no_locations_loaded',
+            ),
+            floatingActionButton:
+                errorLoading && !isLoading && locations.isEmpty
+                    ? FloatingActionButton(
+                        onPressed: context.read<LocationsBloc>().loadNextPage,
+                        child: const Icon(
+                          Icons.refresh,
+                        ),
+                      )
+                    : null,
+          );
+        },
+      ),
+    );
+  }
+}
