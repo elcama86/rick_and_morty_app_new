0f01c64aaaa20c1c3ed18a6d4863d7649ee8cfea
Hector Montilla <elcama_86@msn.com>
2023-11-06 11:27:20 -0400

    location utils added

--- lib/features/locations/infrastructure/infrastructure.dart
+++ lib/features/locations/infrastructure/infrastructure.dart
@@ -2,3 +2,4 @@ export 'datasources/locations_datasource_impl.dart';
 export 'mappers/location_mappers.dart';
 export 'models/models.dart';
 export 'repositories/locations_repository_impl.dart';
+export 'utils/location_utils.dart';
