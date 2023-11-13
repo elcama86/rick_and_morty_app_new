a69ae6ab1e87ea5454680958b0e39cb1b85e1716
Hector Montilla <elcama_86@msn.com>
2023-11-13 12:49:03 -0400

    residents by location bloc file added

--- lib/features/locations/presentation/blocs/blocs.dart
+++ lib/features/locations/presentation/blocs/blocs.dart
@@ -1,3 +1,4 @@
 export 'location_cubit/location_cubit.dart';
 export 'locations_bloc/locations_bloc.dart';
+export 'residents_by_location_bloc/residents_by_location_bloc.dart';
 export 'search_locations_bloc/search_locations_bloc.dart';
